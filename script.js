d797e3d9a87d159030c9cd513977ae901d989bd0
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-12 21:17:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function shortcut(s1, s2) {
-	if(s1=""|| s2=""){
-		return "";
-	}else{
-		return s1[0]+s2[0];
-	}
+	// let a="";
+	// if(s1!='' || s2!=''){
+	// 	return '';
+	// }
+	// else
+	// {
+	// 	a=s1.charAt+ s2.charAt(0);
+	// 	return a;
+	// }
+	return s1[0]+s2[0];
 }
+
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
